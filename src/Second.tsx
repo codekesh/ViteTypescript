921b9ea9ec46211cfab6c8af50c304b1f4b46010
codekesh <201117@iiitt.ac.in>
2023-05-21 23:06:50 +0530

    Second Page Component 1 done

--- src/Second.tsx
+++ src/Second.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 200,
+    editable: true,
+  },
+  {
+    field: "body",
+    headerName: "Body",
+    width: 200,
+    editable: true,
+  },
+];
 
 const Second = () => {
-  return (
-    <div>
-        <h1>Hello</h1>
-    </div>
-  )
-}
+  const [records, setRecords] = useState([] as any);
 
-export default Second
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setRecords(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <Box sx={{ height: 370, width: "100%" }}>
+        <DataGrid
+          rows={records}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+    </>
+  );
+};
+
+export default Second;
